554de7dbbd28ecf4afc3e878a06dcbb9d9dce48b
fuaus <kotingfunk@gmail.com>
2022-05-23 18:47:47 -0400

    add pseudo code

---
+++ Assets/quiz.js
@@ -0,0 +1,12 @@
+/*
+step 1. display start page - title and paragraph and start button.
+
+step 2. timer will start when the start button is clciked, display question, 4 answer buttons
+, start pge becomes hidden then only display question page. When one of the answers is clicked, show correct or wrong. Each question should be 15 seconds and since you have 5 questions. Total time remaining is 75 seconds for 5 questions. 
+When you get a wrong answer your time gets dedcuted by 15 seconds off the timer. IF you get it right, then no penalty. 
+
+step 3. Once you answer all the questions, you will be presented with the score and input your initial and submit button. The timer should stop and time left becomes your score. 
+
+
+
+*/
