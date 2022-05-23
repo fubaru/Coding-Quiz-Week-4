02661aed2ac7ab45639372186a644fcbf3295ca2
fuaus <kotingfunk@gmail.com>
2022-05-23 18:52:10 -0400

    edit pseudo code

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -1,8 +1,9 @@
 /*
 step 1. display start page - title and paragraph and start button.
+start button - ( triggers the quiz game and displays timer and question page)
 
-step 2. timer will start when the start button is clciked, display question, 4 answer buttons
-, start pge becomes hidden then only display question page. When one of the answers is clicked, show correct or wrong. Each question should be 15 seconds and since you have 5 questions. Total time remaining is 75 seconds for 5 questions. 
+step 2. Displays questions page and hides start page - timer will start when the start button is clciked, display question, 4 answer buttons
+, start pge becomes hidden then only display question page. When one of the answers is clicked, show correct or wrong. Each question should be 15 seconds and since you have 5 questions. Total time remaining is 75 seconds for 5 questions. total = number of Questions*15seconds. 
 When you get a wrong answer your time gets dedcuted by 15 seconds off the timer. IF you get it right, then no penalty. 
 
 step 3. Once you answer all the questions, you will be presented with the score and input your initial and submit button. The timer should stop and time left becomes your score. 
